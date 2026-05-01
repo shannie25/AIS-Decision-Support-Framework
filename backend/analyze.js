429f084eac0ca361d9ab8f7b600ab34623be0d01
shannie25 <annie.mamitag25@gmail.com>
2026-05-02 01:02:19 +0800

    Replace project with HTML version

--- backend/analyze.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { analyzeController } = require('./analyzeController');
-
-// POST /api/analyze
-// Accepts institutional profile and returns scored results
-router.post('/', analyzeController);
-
-module.exports = router;
